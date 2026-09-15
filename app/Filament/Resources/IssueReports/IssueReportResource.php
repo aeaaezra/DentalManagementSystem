0e02c91b52a47448fe0bd18a42cdf717523d16ca
EZRAFRIZ <markandrewgella@.com>
2026-09-15 21:47:24 +0800

    Update code

--- app/Filament/Resources/IssueReports/IssueReportResource.php
+++ app/Filament/Resources/IssueReports/IssueReportResource.php
@@ -11,6 +11,7 @@ use App\Filament\Resources\IssueReports\Schemas\IssueReportInfolist;
 use App\Filament\Resources\IssueReports\Tables\IssueReportsTable;
 use App\Models\IssueReport;
 use BackedEnum;
+use UnitEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
@@ -22,7 +23,7 @@ class IssueReportResource extends Resource
 
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBugAnt;
-
+    protected static string|UnitEnum|null $navigationGroup = 'Appointments';
     protected static ?string $navigationLabel = 'Issue Reports';
 
     protected static ?string $modelLabel = 'Issue Report';
